83b97ef674852059703c8cdf8a4b25657d626499
James Volmert <james.volmert@gmail.com>
2016-03-26 11:18:56 -0600

    successful ajax request

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+$(document).on('ready', function() {
+   console.log('sanity check');
 
+
+    var request = $.ajax({
+        method: 'GET',
+        url: 'https://api.open.fec.gov/v1/elections/?election_full=true&page=1&office=president&cycle=2016&per_page=20&api_key=NBb0tBT0yOZcaz2kD9qXjrAbFqpEMCUOSxE7cY55&sort=-total_receipts',
+        //dataType: 'jsonp'
+
+    });
+
+    request.done(function(data) {
+        console.log(data);
+    });
+});
